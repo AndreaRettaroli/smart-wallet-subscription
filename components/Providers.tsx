5285ac76ee158e6830eae5d91807b1bba493c900
hipix345 <feka.7h@skiff.com>
2024-06-23 14:39:02 +0200

    add SessionProvider

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,16 +1,19 @@
-"use client"
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
-import { config } from '@/lib/config'
- 
-export function Providers({ children }: { children: React.ReactNode}) {
-  const queryClient = new QueryClient()
- 
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/lib/config";
+import { SessionProvider } from "next-auth/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
+    <SessionProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </WagmiProvider>
+    </SessionProvider>
+  );
 }
